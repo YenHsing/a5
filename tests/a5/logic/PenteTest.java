f2d0817a1ebb7b1bfca68f9f7ace986d3fa4e191
YenHsing <31437399+YenHsing@users.noreply.github.com>
2022-10-26 17:40:47 -0400

    test update

---
+++ tests/a5/logic/PenteTest.java
@@ -0,0 +1,122 @@
+package a5.logic;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import a5.util.PlayerRole;
+import org.junit.jupiter.api.Test;
+
+class PenteTest {
+    @Test
+    void testConstructor() {
+        // TODO 1: write at least 1 test case
+    }
+
+    @Test
+    void testCopyConstructor() {
+        // test case 1: can a board state be copied to an equal state
+        Pente game1 = new Pente();
+        game1.makeMove(new Position(2, 2));
+        Pente game2 = new Pente(game1);
+        assertTrue(game1.stateEqual(game2));
+
+        // TODO 2: write at least 3 test cases
+    }
+
+    @Test
+    void testHashCode() {
+        Pente game1 = new Pente();
+        Pente game2 = new Pente();
+        Pente game3 = new Pente();
+
+        // test case 1: do two equal nonempty board states have the same hash code
+        game1.makeMove(new Position(3, 3));
+        game2.makeMove(new Position(3, 3));
+        assertEquals(game1.hashCode(), game2.hashCode());
+
+        // test case 2: non-equal board states should be very unlikely to have the
+        // same hash code.
+        game3.makeMove(new Position(0, 0));
+        assertNotEquals(game1.hashCode(), game3.hashCode());
+
+        // TODO 3: write at least 3 test cases
+    }
+
+    @Test
+    void makeMove() {
+        // test case 1: a simple move
+        Pente game = new Pente();
+        Position p = new Position(2, 2);
+        game.makeMove(p); // a move by the first player
+        assertEquals(game.currentPlayer(), PlayerRole.SECOND_PLAYER);
+        assertEquals(game.currentTurn(), 2);
+        assertFalse(game.hasEnded());
+        assertEquals(game.capturedPairsNo(PlayerRole.FIRST_PLAYER), 0);
+        assertEquals(game.capturedPairsNo(PlayerRole.SECOND_PLAYER), 0);
+        assertEquals(game.board().get(p), PlayerRole.FIRST_PLAYER.boardValue());
+
+        // test case 2: try a capture
+        game.makeMove(new Position(2, 3)); // a move by the second player
+        game.makeMove(new Position(3, 2)); // a move by the first player
+        game.makeMove(new Position(2, 4)); // a move by the second player
+        game.makeMove(new Position(2, 5)); // a move by the first player, which should capture the pair [(2, 3), (2, 4)]
+        assertEquals(game.capturedPairsNo(PlayerRole.FIRST_PLAYER), 1);
+        assertEquals(game.board().get(new Position(2, 3)), 0); // the stone should be removed
+        assertEquals(game.board().get(new Position(2, 4)), 0); // the stone should be removed
+
+        // TODO 4: write at least 3 test cases
+    }
+
+    @Test
+    void capturedPairsNo() {
+        // test case 1: are captured pairs registered?
+        Pente game = new Pente();
+        game.makeMove(new Position(3, 2)); // a move by the first player
+        game.makeMove(new Position(3, 3)); // a move by the second player
+        game.makeMove(new Position(4, 2)); // a move by the first player
+        game.makeMove(new Position(3, 4)); // a move by the second player
+        game.makeMove(new Position(3, 5)); // a move by the first player, which should capture the pair [(2, 3), (2, 4)]
+        assertEquals(game.capturedPairsNo(PlayerRole.FIRST_PLAYER), 1);
+
+        // TODO 5: write at least 3 test cases
+    }
+
+    @Test
+    void hasEnded() {
+        // test case 1: is a board with 5 in a row an ended game?
+        Pente game = new Pente();
+        assertFalse(game.hasEnded());
+        game.makeMove(new Position(1, 1)); // a move by the first player
+        game.makeMove(new Position(2, 1)); // a move by the second player
+        game.makeMove(new Position(1, 2)); // a move by the first player
+        game.makeMove(new Position(2, 2)); // a move by the second player
+        game.makeMove(new Position(1, 3)); // a move by the first player
+        game.makeMove(new Position(2, 3)); // a move by the second player
+        game.makeMove(new Position(1, 4)); // a move by the first player
+        game.makeMove(new Position(2, 4)); // a move by the second player
+        game.makeMove(new Position(1, 5)); // a move by the first player
+        assertTrue(game.hasEnded());
+
+        // TODO 6: write at least 3 test cases
+    }
+
+    @Test
+    void stateEqual() {
+        Pente game1 = new Pente();
+        Pente game2 = new Pente();
+        Pente game3 = new Pente();
+
+        // test 1: games with equal board states should be stateEqual()
+        game1.makeMove(new Position(3, 3));
+        game2.makeMove(new Position(3, 3));
+        assertTrue(game1.stateEqual(game2));
+        assertTrue(game2.stateEqual(game1));
+
+        // test 2: games with unequal board states should not be stateEqual()
+        game3.makeMove(new Position(0, 0));
+        assertFalse(game1.stateEqual(game3));
+        // TODO 7: write at least 3 test cases
+    }
+}
